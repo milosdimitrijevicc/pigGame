aa37fccf14904e94dd00de34d83091423017d2ae
Milos Dimitrijevic <dimitrijevic.milos40@gmail.com>
2024-03-05 14:54:04 +0100

    adding functionality to game part #1

---
+++ script.js
@@ -0,0 +1,19 @@
+//Igra se igra do 20
+//Player 1 obrce kocku koliko god hoce puta i svaki rezultat koji dobije se sabira sa rezultatom trenutne runde
+//Ako igrac izvuce broj 1 svi poeni iz te runde se gube
+//Ukoliko igrac odluci da zadrzi trenutne poene pre nego sto dobije broj 1, poeni dobijeni u rundi se sabiraju u totalni skor
+let player1Round = 0;
+let player1Total = 0;
+
+let player2Round = 0;
+let player2Total = 0;
+
+const dice1 = 1;
+const dice2 = 2;
+const dice3 = 3;
+const dice4 = 4;
+const dice5 = 5;
+const dice6 = 6;
+
+
+
