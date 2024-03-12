882db2bce34f3f0b0d71d2c871467d67392a7595
Milos Dimitrijevic <dimitrijevic.milos40@gmail.com>
2024-03-12 20:00:55 +0100

    rebuilding code

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
-'use strict';
+const game = () => {
 
-//Selecting elements
-const score0El = document.querySelector('#score--0');
-const score1El = document.querySelector('#score--1');
-const current0El = document.querySelector('#current--0');
-const current1El = document.querySelector('#current--1');
+  //Current Score
+  const player1CurrentScore = document.querySelector('#current--1');
+  const player2CurrentScore = document.querySelector('#current--2');
+  //Total Score
+  const player1TotalScore = document.querySelector('#score--1');
+  const player2TotalScore = document.querySelector('#score--2');
+  //Buttons
+  const btnNewGame = document.querySelector('#btn--new');
+  const btnRoll = document.querySelector('#btn--roll');
+  const btnHold = document.querySelector('#btn--hold');
 
 
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+  //Getters
+  const getPlayer1CurrentScore = player1CurrentScore;
+  const getPlayer2CurrentScore = player2CurrentScore;
+  const getPlayer1TotalScore = player1TotalScore;
+  const getPlayer2TotalScore = player2TotalScore;
 
-//Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-let currentScore = 0;
-
-//Rolling dice functionality
-btnRoll.addEventListener('click' , function(){
-  
-  //1. Generate a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
-
-  //2. Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`
-
-  //3. Check for rolled dice 1
-  if (dice !== 1){
-    //Add dice to current score
-    currentScore = currentScore + dice;
-    current0El.textContent = currentScore; //CHANGE LATER
-  } else {
-  //Switch to next player
-  }
-})
+  //Setters
+  const setPlayer1CurrentScore = (value) => player1CurrentScore = player1CurrentScore + value;
+  const setPlayer2CurrentScore = (value) => player2CurrentScore = player2CurrentScore + value;
+  const setPlayer1TotalScore = (value) => player1TotalScore = player1TotalScore + value;
+  const setPlayer2TotalScore = (value) => player2TotalScore = player2TotalScore + value;
+}
